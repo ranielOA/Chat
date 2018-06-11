721a8d28b893877f3978894c6d98d7637a70a5c1
Raniel <r.a_tw@hotmail.com>
2018-06-10 23:08:41 -0300

    pequena modificação

--- app/src/main/java/br/com/raniel/chat/activity/MainActivity.java
+++ app/src/main/java/br/com/raniel/chat/activity/MainActivity.java
@@ -1,9 +1,11 @@
 package br.com.raniel.chat.activity;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
 import android.widget.ListView;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.List;
 
@@ -20,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
 
         ListView lvConversa = findViewById(R.id.main_listview_conversa);
 
-        List<Mensagem> mensagens = Arrays.asList(new Mensagem("ola babaca", 1), new Mensagem("cuzao du garai", 2));
+        List<Mensagem> mensagens = Arrays.asList(new Mensagem("ola, tudo bem?", 1), new Mensagem("sim, de boas", 2));
 
         MensagemAdapter mensagemAdapter = new MensagemAdapter(this, mensagens, 2);
 
