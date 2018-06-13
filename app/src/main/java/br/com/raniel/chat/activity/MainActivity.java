b28dd49e2a2e7185a760be3119c5f424eb25aff5
Raniel <r.a_tw@hotmail.com>
2018-06-12 21:33:19 -0300

    adicionando EventBus

--- app/src/main/java/br/com/raniel/chat/activity/MainActivity.java
+++ app/src/main/java/br/com/raniel/chat/activity/MainActivity.java
@@ -10,6 +10,9 @@ import android.widget.ListView;
 
 import com.squareup.picasso.Picasso;
 
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,6 +24,7 @@ import br.com.raniel.chat.app.ChatApplication;
 import br.com.raniel.chat.callback.EnviarMensagemCallback;
 import br.com.raniel.chat.callback.OuvirMensagemCallBack;
 import br.com.raniel.chat.component.ChatComponent;
+import br.com.raniel.chat.event.MensagemEvent;
 import br.com.raniel.chat.model.Mensagem;
 import br.com.raniel.chat.service.ChatService;
 import butterknife.BindView;
@@ -67,7 +71,16 @@ public class MainActivity extends AppCompatActivity {
 
         picasso.get().load("https://api.adorable.io/avatars/285/" + idDoUsuario + ".png").into(avatar);
 
-        ouvirMensagem();
+        ouvirMensagem(new MensagemEvent(null));
+
+        EventBus.getDefault().register(this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        EventBus.getDefault().unregister(this);
     }
 
     @OnClick(R.id.main_enviar)
@@ -76,14 +89,16 @@ public class MainActivity extends AppCompatActivity {
         chatService.enviar(msg).enqueue(new EnviarMensagemCallback());
     }
 
-    public void colocaNaLista(Mensagem mensagem) {
-        mensagens.add(mensagem);
+    @Subscribe
+    public void colocaNaLista(MensagemEvent mensagemEvent) {
+        mensagens.add(mensagemEvent.mensagem);
         MensagemAdapter adapter = new MensagemAdapter(this, mensagens, idDoUsuario);
         lvListaDeMensagens.setAdapter(adapter);
     }
 
-    public void ouvirMensagem() {
+    @Subscribe
+    public void ouvirMensagem(MensagemEvent mensagemEvent) {
         Call<Mensagem> mensagemCall = chatService.ouvirMensagens();
-        mensagemCall.enqueue(new OuvirMensagemCallBack(this));
+        mensagemCall.enqueue(new OuvirMensagemCallBack());
     }
 }
--- app/src/main/java/br/com/raniel/chat/callback/OuvirMensagemCallBack.java
+++ app/src/main/java/br/com/raniel/chat/callback/OuvirMensagemCallBack.java
@@ -1,30 +1,26 @@
 package br.com.raniel.chat.callback;
 
+import org.greenrobot.eventbus.EventBus;
+
 import br.com.raniel.chat.activity.MainActivity;
+import br.com.raniel.chat.event.MensagemEvent;
 import br.com.raniel.chat.model.Mensagem;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class OuvirMensagemCallBack implements Callback<Mensagem> {
-    private MainActivity activity;
-
-    public OuvirMensagemCallBack(MainActivity activity) {
-        this.activity = activity;
-    }
 
     @Override
     public void onResponse(Call<Mensagem> call, Response<Mensagem> response) {
         if (response.isSuccessful()){
             Mensagem mensagem = response.body();
 
-            activity.colocaNaLista(mensagem);
-            activity.ouvirMensagem();
+            EventBus.getDefault().post(new MensagemEvent(mensagem));
         }
     }
 
     @Override
     public void onFailure(Call<Mensagem> call, Throwable t) {
-            activity.ouvirMensagem();
     }
 }
